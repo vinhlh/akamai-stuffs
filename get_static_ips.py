dacefcfe7d9760f98f44f24f7bdfd2933775a1ae
Vinh Le <vinh.le@vn.zalora.com>
2016-04-07 13:51:29 +0700

    add thumbor support

--- get_static_ips.py
+++ get_static_ips.py
@@ -2,13 +2,27 @@ import socket
 
 countries = ['hk', 'id', 'my', 'ph', 'sg', 'th', 'tw', 'vn']
 
-def get_hosts(country, number):
+def get_static_hosts(country, number):
     name = country + number
-    return 'zstatic' + name + '-a.akamaihd.net.edgesuite-staging.net', 'zstatic' + name + '-a.akamaihd.net'
+    return [
+        'zstatic' + name + '-a.akamaihd.net.edgesuite-staging.net',
+        'zstatic' + name + '-a.akamaihd.net'
+    ]
 
-for country in countries:
-    hosts = [get_hosts(country, '01'), get_hosts(country, '02')]
+def get_thumbor_host(number):
+    return [
+        'zthumbor' + number + '-a.akamaihd.net.edgesuite-staging.net',
+        'n' + number[1] + '-zthumbor-a.akamaihd.net'
+    ]
 
+def get_results(hosts):
     for ping_server, host_name in hosts:
         ip = socket.gethostbyname(ping_server)
-        print ip, host_name
+        print ip, ping_server, host_name
+
+for country in countries:
+    static_hosts = [get_static_hosts(country, '01'), get_static_hosts(country, '02')]
+    get_results(static_hosts)
+
+thumbor_hosts = [get_thumbor_host('01'), get_thumbor_host('02')]
+get_results(thumbor_hosts)
--- test_aic.py
+++ test_aic.py
@@ -21,7 +21,7 @@ headers = {
 }
 
 def get_alice(domain):
-    return 'http://www.zalora.' + domain
+    return 'http://www.zalora.' + domain + '/all-products/'
 
 def validate_image(image):
     response = requests.post(image, cookies = cookies, headers = headers)
@@ -30,15 +30,19 @@ def validate_image(image):
         return result.group(1)
     return None
 
-for domain in domains:
-    response = requests.get(get_alice(domain))
-    # src="http://zstaticvn02-a.akamaihd.net/cms/images/120K.jpg"
-    image_result = re.search('="(http://zstatic[a-z]{2}[0-9]{2}-a.akamaihd.net/[^\"]+.jpg)"', response.text)
+def validate(image_url_pattern, type):
+    image_result = re.search(image_url_pattern, response.text)
     if image_result:
         image_quality = validate_image(image_result.group(1))
-        # print domain, image_quality, 'Passed' if image_quality == '60' else 'Failed'
-        print "{0:10} {1:5}  {2}".format(domain, image_quality, 'Passed' if image_quality == '60' else 'Failed')
+        print "{0:10} {1:10} {2:5}  {3}".format(domain, type, image_quality, 'Passed' if image_quality == '60' else 'Failed')
     else:
-        print "{0:10} error".format(domain)
+        print "{0:10} {1:10} error".format(domain, type)
+
+
+for domain in domains:
+    response = requests.get(get_alice(domain))
+
+    validate('="(http://zstatic[a-z]{2}[0-9]{2}-a.akamaihd.net/[^\"]+.jpg)"', 'static')
+    validate('"(http://n[1-2]{1}-zthumbor-a.akamaihd.net/[^\"]+.jpg)"', 'thumbor')
 
 
